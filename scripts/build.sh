975cf4179296514a5ad1fe6d29c65b60846f288d
Jassiel O. <jassielovando@protonmail.com>
2025-04-05 17:23:15 -0400

    build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,6 @@
-#!/bin/bash
-
-set -e
-
-# Find all .cpp files
 FILES=$(find actividades -name "*.cpp")
 echo "Found $(echo "$FILES" | wc -l) files"
 
-# Create a compilation commands file
 > compile_commands.txt
 for file in $FILES; do
   echo "clang++ -O2 -o /dev/null $file" >> compile_commands.txt
